032d1f288f998ec6ec55d4fd3114a9af26aca25e
Abimael Colque <colqueabimael@gmail.com>
2025-08-02 20:00:53 -0400

    rutas configuradas para productos

--- backend/app/Http/Controllers/ProductoController.php
+++ backend/app/Http/Controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
@@ -11,7 +12,7 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Producto::all());
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,8 +2,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('marcas', MarcaController::class);
+Route::apiResource('productos', ProductoController::class);
